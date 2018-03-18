333060e5275b735678da4ba815d9f6fea6f60f32
Dinesh <flexdinesh@gmail.com>
2018-03-19 00:54:25 +0530

    Make header sticky

--- react-landing/src/components/Nav/Nav.js
+++ react-landing/src/components/Nav/Nav.js
@@ -4,6 +4,33 @@ import { toElement as scrollToElement } from '@utils/scroll';
 import './style.scss';
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.state = {
+      isSticky: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll() {
+    if(window.pageYOffset > this.nav.offsetTop) {
+      this.setState({
+        isSticky: true
+      });
+    } else {
+      this.setState({
+        isSticky: false
+      });
+    }
+  }
 
   scrollToPage(pageSelector) {
     const nextPage = document.querySelector(pageSelector);
@@ -11,12 +38,12 @@ class Nav extends Component {
   }
 
   render() {
+    const stickyClass = this.state.isSticky ? 'sticky' : '';
     return (
-      <nav>
+      <nav className={ stickyClass } ref={elem => {this.nav = elem}}>
         <div className="menu">
           <div className="menu__item active" onClick={ e => this.scrollToPage('.about-page')}>About</div>
           <div className="menu__item" onClick={ e => this.scrollToPage('.portfolio-page')}>Portfolio</div>
-          {/* <div className="menu__item" onClick={ e => this.scrollToPage('.timeline-page')}>Blogs</div> */}
         </div>
       </nav>
     );
