5f57d933997e3a8e0f2243cdaafe843f443d59b5
Dinesh <flexdinesh@gmail.com>
2018-03-20 00:05:40 +0530

    About page: Mock content

--- react-landing/src/pages/AboutPage/AboutPage.js
+++ react-landing/src/pages/AboutPage/AboutPage.js
@@ -14,7 +14,9 @@ class AboutPage extends Component {
               <img src={PicSource} alt="pic" />
             </div>
             <div className="about-content">
-              <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+              <p>I am a popular bla bla blaaaaah. I go blah and do blah blah blah. I bla bla bla of all the awesomatic blahs.</p>
+              <p>The biggess of all the jajajas are my hats in my bin.</p>
+              <p className='text-emoji'>\ (•◡•) /</p>
             </div>
           </div>        
           
