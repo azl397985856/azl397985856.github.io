ba7fb71844505e000f7a591080c099a3d838a0ac
Dinesh <flexdinesh@gmail.com>
2018-04-01 01:20:11 +0530

    Remove theme references and define breakpoint mixins

--- react-landing/src/index.js
+++ react-landing/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './libs/fontawesome/fontawesome-all.min';
-import './styles/reset.scss';
+import './styles/base.scss';
 import './styles/global.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
