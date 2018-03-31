3879f5b43719037c2a1b70a992c97122c86f01b9
Dinesh <flexdinesh@gmail.com>
2018-04-01 02:05:27 +0530

    Refactor global styles into sass partial

--- react-landing/src/index.js
+++ react-landing/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './libs/fontawesome/fontawesome-all.min';
 import './styles/base.scss';
-import './styles/global.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
