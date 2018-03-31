bacb6dcb24206cd096f5033b800f010b0027070b
Dinesh <flexdinesh@gmail.com>
2018-03-31 18:32:10 +0530

    Remove state test files

--- react-landing/src/pages/LandingPage/LandingPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import LandingPage from './LandingPage';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<LandingPage />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
