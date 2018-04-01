c77e0fe62898bf48d5b1d07d530acdd3d5368cd2
Dinesh <flexdinesh@gmail.com>
2018-04-01 17:46:54 +0530

    Remove service worker

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
-const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
 const ignoredFiles = require('react-dev-utils/ignoredFiles');
 const config = require('./webpack.config.dev');
 const paths = require('./paths');
@@ -84,12 +83,6 @@ module.exports = function(proxy, allowedHost) {
     before(app) {
       // This lets us open files from the runtime error overlay.
       app.use(errorOverlayMiddleware());
-      // This service worker file is effectively a 'no-op' that will reset any
-      // previous service worker registered for the same host:port combination.
-      // We do this in development to avoid hitting the production cache if
-      // it used the same host and port.
-      // https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
-      app.use(noopServiceWorkerMiddleware());
     },
   };
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './libs/fontawesome/fontawesome-all.min';
 import './styles/base.scss';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
