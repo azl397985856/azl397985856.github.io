0a3a80ff6652702beec158001b8909e08d799f2f
luzhipeng <luzhipeng@duiba.com.cn>
2018-09-05 12:08:38 +0800

    fix: 改为hashRouter

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/HomePage/index";
 import ToolPage from "./pages/ToolPage/index";
 
